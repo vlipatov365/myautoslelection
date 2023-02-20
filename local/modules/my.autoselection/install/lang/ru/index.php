976521da96d0d94bfc852ff67df0ecfbd002b10e
Вячеслав Липатов <vlipatov365@yandex.ru>
2023-02-20 21:26:56 +0300

    Собрал первичную структуру файлов, Начал работу над install/index.php

---
+++ local/modules/my.autoselection/install/lang/ru/index.php
@@ -0,0 +1,5 @@
+<?php
+$MESS['MODULE_NAME'] = 'Новый модуль авто-подбора';
+$MESS['MODULE_DESCRIPTION'] = 'Простой модуль авто-подбора';
+$MESS['PARTNER_NAME'] = 'Я';
+$MESS['PARTNER_URI'] = 'my.my';
