bb55ae7865b7146d8446995a38d13cf472aeb510
Вячеслав Липатов <vlipatov365@yandex.ru>
2023-02-28 21:22:33 +0300

    Добавлен компонент с файлов класса и дефолтным шаблоном

---
+++ local/modules/my.autoselection/install/components/my/autoselection.grid/class.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace My\AutoSelection;
+
+use Bitrix\Main\Engine\Contract\Controllerable;
+class AutoselectionComponent extends \CBitrixComponent
+    implements Controllerable
+{
+    public function configureActions()
+    {
+        // TODO: Implement configureActions() method.
+    }
+}
