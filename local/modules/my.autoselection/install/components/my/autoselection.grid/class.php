f035a43083d8f32819de5712f15a2527fb26115a
Вячеслав Липатов <vlipatov365@yandex.ru>
2023-03-01 17:46:57 +0300

    В templates/.default добавлены bootstrap сетка и вывод элементов в $arResult[ROWS]. В class.php методы получения элементов, свойств и их преобразования

--- local/modules/my.autoselection/install/components/my/autoselection.grid/class.php
+++ local/modules/my.autoselection/install/components/my/autoselection.grid/class.php
@@ -3,11 +3,167 @@
 namespace My\AutoSelection;
 
 use Bitrix\Main\Engine\Contract\Controllerable;
+use Bitrix\Main\Loader;
+use My\AutoSelection\Helper;
+use My\AutoSelection\Helpers\Iblock;
+use My\Autoselection\Helpers\HighloadBlock;
+
 class AutoselectionComponent extends \CBitrixComponent
     implements Controllerable
 {
+    const IBLOCK_CODE = 'my_autoselection';
+    const  HIGHLOADBLOCK_NAME = 'MyAutoSelectionBrands';
+
+    public function executeComponent()
+    {
+        $this->makeHeaders();
+        $this->makeFilter();
+        $this->getData();
+        $this->includeComponentTemplate();
+    }
+
     public function configureActions()
     {
-        // TODO: Implement configureActions() method.
+
+    }
+
+    public function getData()
+    {
+        $arValues = [];
+        $arSelect = $this->makeSelect();
+        foreach ($arSelect as $select) {
+            $arValues[] = $select . '_VALUE';
+        }
+        Loader::includeModule('my.autoselection'); //TODO здесь должна быть проверка
+        $IBlockID = Iblock::getIblockId(['CODE' => self::IBLOCK_CODE]); //TODO вынести в конструктор
+        $IBlockList = \CIBlockElement::GetList(
+            [],
+            ['IBLOCK_ID' => $IBlockID],
+            false,
+            false,
+            $arSelect
+        );
+        while ($element = $IBlockList->Fetch()) {
+            foreach ($arValues as $value) {
+                if (isset($element[$value])) {
+                    $row[$value] = $element[$value];
+                }
+            }
+            $this->arResult['ROWS'][] = $row;
+        }
+        return $this->arResult['ROWS'];
+    }
+
+    /**
+     * Метод преобразования свойств элементов для запроса GetList()
+     * @return array|string[]
+     */
+    public function makeSelect()
+    {
+        $arSelect = [];
+        $properties = $this->getProperty();
+        foreach ($properties as $property) {
+            $arSelect[] = 'PROPERTY_' . $property['id'];
+        }
+//        $arSelect = array_merge($arSelect, ['ID']);
+        return $arSelect;
+    }
+
+    /**
+     * Метод получения свойств элементов
+     * @return array|string[]
+     */
+    public function getProperty()
+    {
+        $properties = [];
+        $iBlockId = Iblock::getIblockId(['CODE' => self::IBLOCK_CODE]); //TODO вынести в конструктор
+        $result = \CIBlockElement::GetProperty(
+            $iBlockId,
+            []
+        );
+        while ($property = $result->Fetch()) {
+            $properties[] = [
+                'id' => $property['ID'],
+                'name' => $property['NAME'],
+                'code' => $property['CODE'],
+                'sort' => $property['SORT']
+            ];
+        }
+        return $properties;
+    }
+
+    /**
+     * Формирование массива заголовков
+     * @return array
+     */
+    public function makeHeaders()
+    {
+        $headers = [];
+        $properties = $this->getProperty();
+        foreach ($properties as $property) {
+            $headers[] = $property['name'];
+        }
+        return $this->arResult['HEADERS'] = $headers;
+    }
+
+    /**
+     * Формирование массива фильтра и полей
+     * @return void
+     */
+    public function makeFilter()
+    {
+        $filterItem = [];
+        $fieldsMap = $this->getProperty();
+        foreach ($fieldsMap as $field) {
+            $filterItem = [
+                'id' => 'PROPERTY_' . $field['id'],
+                'name' => $field['name'],
+                'code' => $field['code'],
+                'default' => 1
+            ];
+            switch ($filterItem['code']) {
+                case 'MY_AUTOSELECTION_BRANDS':
+                    $filterItem['type'] = 'list';
+                    $filterItem['items'] = $this->getBrands();
+                    $filterItem['params'] = ['multiple' => 'Y'];
+                    break;
+                case 'MY_AUTOSELECTION_CONDITION':
+                    $filterItem['type'] = 'list';
+                    $filterItem['items'] = ['Поддержанный' => 'Поддержанный', 'Новое' => 'Новое'];
+                    $filterItem['params'] = ['multiple' => 'N'];
+                    break;
+                case 'MY_AUTOSELECTION_PRICE':
+                    $filterItem['type'] = 'number';
+                    break;
+                case 'MY_AUTOSELECTION_YEAR':
+                    $filterItem['type'] = 'number';
+                    break;
+                case 'MY_AUTOSELECTION_RAIN_SENSOR':
+                    $filterItem['type'] = 'list';
+                    $filterItem['items'] = ['Да' => 'Да', 'Нет' => 'Нет'];
+                    $filterItem['params'] = ['multiple' => 'N'];
+                    break;
+            };
+            $this->arResult['FILTER'][] = $filterItem;
+            $this->arResult['FILEDS_MAP'][] = $filterItem;
+        }
+    }
+
+    /**
+     * Список брендов из highload-блока
+     * @return array|void
+     * @throws \Bitrix\Main\SystemException
+     */
+    public function getBrands()
+    {
+        $hlBlockId = HighloadBlock::getHlBlockId(['NAME' => self::HIGHLOADBLOCK_NAME]);
+
+        $hlBLockClass = HighloadBlock::getEntityDataClass($hlBlockId);
+        $hlBLockList = $hlBLockClass::GetList();
+        while ($el = $hlBLockList->Fetch()) {
+            $arBrands[$el['UF_BRANDNAME']] = $el['UF_BRANDNAME'];
+        }
+        if (isset($arBrands) && !empty($arBrands))
+            return $arBrands;
     }
 }
--- local/modules/my.autoselection/install/components/my/autoselection.grid/templates/.default/template.php
+++ local/modules/my.autoselection/install/components/my/autoselection.grid/templates/.default/template.php
@@ -1,2 +1,69 @@
 <?php
-echo 'Привет, Слава';
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <h3 class="text-center">
+                h3. Lorem ipsum dolor sit amet.
+            </h3>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <table class="table table-hover">
+                <thead>
+                <tr>
+                    <?php foreach ($arResult['HEADERS'] as $header): ?>
+                        <th>
+                            <?= $header ?>
+                        </th>
+                    <?php endforeach; ?>
+                </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($arResult['ROWS'] as $row): ?>
+                    <tr>
+                        <?php foreach ($row as $value): ?>
+                        <td>
+                            <?= $value ?>
+                        </td>
+                        <?php endforeach; ?>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <nav>
+                <ul class="pagination">
+                    <li class="page-item">
+                        <a class="page-link" href="#">Previous</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">1</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">2</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">3</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">4</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">5</a>
+                    </li>
+                    <li class="page-item">
+                        <a class="page-link" href="#">Next</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
+
